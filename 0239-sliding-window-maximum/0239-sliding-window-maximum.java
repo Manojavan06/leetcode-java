fd2ed4629fede76d4ea87a1700b4f54e832f1f09
Manojavan <138390760+Manojavan06@users.noreply.github.com>
2026-07-06 08:54:38 +0530

    Time: 33 ms (33.32%), Space: 156.2 MB (13.12%) - LeetHub - Time: O(n), Space: O(n)

---
+++ 0239-sliding-window-maximum/0239-sliding-window-maximum.java
@@ -0,0 +1,31 @@
+import java.util.*;
+
+class Solution {
+
+    public int[] maxSlidingWindow(int[] nums, int k) {
+
+        Deque<Integer> deque = new LinkedList<>();
+        int[] ans = new int[nums.length - k + 1];
+
+        for (int i = 0; i < nums.length; i++) {
+
+            while (!deque.isEmpty() && deque.peekFirst() <= i - k)
+                deque.pollFirst();
+
+            while (!deque.isEmpty() &&
+                   nums[deque.peekLast()] < nums[i])
+                deque.pollLast();
+
+            deque.offerLast(i);
+
+            if (i >= k - 1)
+                ans[i - k + 1] = nums[deque.peekFirst()];
+        }
+
+        return ans;
+    }
+}
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
