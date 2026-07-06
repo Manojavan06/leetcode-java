b37b31e941b8844ca0f30acc50bc8b9d17c90a59
Manojavan <138390760+Manojavan06@users.noreply.github.com>
2026-07-06 08:33:01 +0530

    Time: 1 ms (99.84%), Space: 47.9 MB (38.09%) - LeetHub - Time: O(n), Space: O(1)

---
+++ 0283-move-zeroes/0283-move-zeroes.java
@@ -0,0 +1,16 @@
+class Solution {
+    public void moveZeroes(int[] nums) {
+        int index = 0;
+
+        for (int num : nums)
+            if (num != 0)
+                nums[index++] = num;
+
+        while (index < nums.length)
+            nums[index++] = 0;
+    }
+}
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
