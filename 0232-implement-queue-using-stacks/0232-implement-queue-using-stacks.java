3cdc8bc1e0e845a9bcf7fe1530de76bd8769f0ca
Manojavan <138390760+Manojavan06@users.noreply.github.com>
2026-07-06 08:53:42 +0530

    Time: 0 ms (100%), Space: 43.1 MB (13.03%) - LeetHub - Time: O(1) amortized, O(n) worst case for pop and peek operations, Space: O(n)

---
+++ 0232-implement-queue-using-stacks/0232-implement-queue-using-stacks.java
@@ -0,0 +1,45 @@
+import java.util.*;
+
+class MyQueue {
+
+    Stack<Integer> in;
+    Stack<Integer> out;
+
+    public MyQueue() {
+        in = new Stack<>();
+        out = new Stack<>();
+    }
+
+    public void push(int x) {
+        in.push(x);
+    }
+
+    public int pop() {
+        peek();
+        return out.pop();
+    }
+
+    public int peek() {
+        if (out.isEmpty()) {
+            while (!in.isEmpty())
+                out.push(in.pop());
+        }
+        return out.peek();
+    }
+
+    public boolean empty() {
+        return in.isEmpty() && out.isEmpty();
+    }
+}
+/**
+ * Your MyQueue object will be instantiated and called as such:
+ * MyQueue obj = new MyQueue();
+ * obj.push(x);
+ * int param_2 = obj.pop();
+ * int param_3 = obj.peek();
+ * boolean param_4 = obj.empty();
+ */
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
