bd6c3948af4424a16fd0b627773befbfc6b06899
Manojavan <138390760+Manojavan06@users.noreply.github.com>
2026-07-10 11:25:48 +0530

    Time: 0 ms (100%), Space: 19.3 MB (23.56%) - LeetHub - Time: O(n), Space: O(n)

---
+++ 0228-summary-ranges/0228-summary-ranges.py
@@ -0,0 +1,26 @@
+class Solution:
+    def summaryRanges(self, nums: List[int]) -> List[str]:
+        ans = []
+        i = 0
+        n = len(nums)
+
+        while i < n:
+            start = nums[i]
+
+            while i + 1 < n and nums[i + 1] == nums[i] + 1:
+                i += 1
+
+            end = nums[i]
+
+            if start == end:
+                ans.append(str(start))
+            else:
+                ans.append(f"{start}->{end}")
+
+            i += 1
+
+        return ans
+
+# Synced seamlessly with LeetHub Pro
+# Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+# Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
